446e3baf0c52121941ccb19e74929430be7d58c7
Siergej.S <135639288+Ssobol7@users.noreply.github.com>
2023-12-04 21:31:11 +0100

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -57,10 +57,10 @@ def create_accounts():
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
 
+
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
-
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -68,21 +68,22 @@ def list_accounts():
     This endpoint will list all Accounts
     """
     app.logger.info("Request to list Accounts")
+
     accounts = Account.all()
     account_list = [account.serialize() for account in accounts]
-    app.logger.info("Returning [%s] accounts", len(account_list))
+    app.logger.info("List all found [%s] account", len(account_list))
+
     return jsonify(account_list), status.HTTP_200_OK
 
 
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
-
 @app.route("/accounts/<int:account_id>", methods=["GET"])
-def get_accounts(account_id):
+def read_account(account_id):
     """
-    Reads an Account
-    This endpoint will read an Account based the account_id that is requested
+    Read an Account
+    This endpoint will read an Account based on the provided account_id
     """
     app.logger.info("Request to read an Account with id: %s", account_id)
 
@@ -93,40 +94,45 @@ def get_accounts(account_id):
     return account.serialize(), status.HTTP_200_OK
 
 
-
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
-
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
-def update_accounts(account_id):
+def update_account(account_id):
     """
-    Update an Account
-    This endpoint will update an Account based on the posted data
+    Update an existing Account
+    This endpoint will update an existing Account based on provided account info and account_id
     """
-    app.logger.info("Request to update an Account with id: %s", account_id)
+    app.logger.info("Request to update account with account_id: %s", account_id)
+    check_content_type("application/json")
+
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-    account.deserialize(request.get_json())
-    account.update()
-    return account.serialize(), status.HTTP_200_OK
+
+    update_account = Account()
+    update_account.deserialize(request.get_json())
+    update_account.id = account_id
+    update_account.update()
+
+    return jsonify(update_account.serialize()), status.HTTP_200_OK
 
 
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
-
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
-def delete_accounts(account_id):
+def delete_account(account_id):
     """
-    Delete an Account
-    This endpoint will delete an Account based on the account_id that is requested
+    Delete an existing Account
+    This endpoint will delete an existing Account based on provided account_id
     """
-    app.logger.info("Request to delete an Account with id: %s", account_id)
+    app.logger.info("Request to delete account with account_id: %s", account_id)
+
     account = Account.find(account_id)
     if account:
         account.delete()
+
     return "", status.HTTP_204_NO_CONTENT
 
 
