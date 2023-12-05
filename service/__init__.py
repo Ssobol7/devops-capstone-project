8c12a48fe75535222e6ebbe9767b76eec0897504
Siergej.S <135639288+Ssobol7@users.noreply.github.com>
2023-12-05 01:55:09 +0100

    Update __init__.py

--- service/__init__.py
+++ service/__init__.py
@@ -6,11 +6,15 @@ and SQL database
 """
 import sys
 from flask import Flask
+from flask_talisman import Talisman
+from flask_cors import CORS
 from service import config
 from service.common import log_handlers
 
 # Create Flask application
 app = Flask(__name__)
+talisman = Talisman(app)
+CORS(app)
 app.config.from_object(config)
 
 # Import the routes After the Flask app is created
